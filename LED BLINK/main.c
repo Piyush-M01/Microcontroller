6d3e13e6a899a3707c2d69c2ade1a9a6a5bf67b7
Piyush-M01 <79186088+Piyush-M01@users.noreply.github.com>
2021-10-12 16:44:48 +0530

    Add files via upload

---
+++ LED BLINK/main.c
@@ -0,0 +1,18 @@
+#ifndef F_CPU
+#define F_CPU 1600000UL
+#endif
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRB |= (1<<DDB5);
+	while (1)
+	{
+		PORTB |= (1<<PORTB5);
+		_delay_ms(10000);
+		PORTB &= ~ (1<<PORTB5);
+		_delay_ms(1000);
+	}
+}
+
